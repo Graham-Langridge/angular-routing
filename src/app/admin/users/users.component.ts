fb53903511f4f638c7b279177135ed7acaadf7cb
Graham Langridge <grahamlangridge@lineone.net>
2019-02-26 21:34:27 +0000

    Tested e2e.

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: any[];
+  hasPermissions = false;
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermissions) {
+        this.getUsers()
+        .then (users => this.users = users)
+        .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+
+  async getUsers() {
+    return [
+      {name: 'John', email: 'john@angular.com'},
+      {name: 'Graham', email: 'graham@angular.com'}
+    ];
   }
 
 }
